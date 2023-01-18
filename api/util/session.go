96f596c24b4c646a0593626a07ff69fc24427f63
Olumide Olugbemiro <olugbemiro.olumide@gmail.com>
2023-01-18 09:38:40 +0100

    updated session to handle saving data and flash messages

--- api/util/session.go
+++ api/util/session.go
@@ -2,25 +2,65 @@ package util
 
 import (
 	"log"
+	"os"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
-func FlashMessage(c *gin.Context, message string) {
-	session := sessions.Default(c)
-	session.AddFlash(message)
-	if err := session.Save(); err != nil {
-		log.Printf("error in flashMessage saving session: %s", err)
-	}
+type Session struct {
+	storage sessions.Session
 }
 
-func Flashes(c *gin.Context) []interface{} {
-	session := sessions.Default(c)
-	flashes := session.Flashes()
+func NewSession(c *gin.Context) Session {
+	return Session{sessions.Default(c)}
+}
+
+func (s *Session) SetSessionData(data string) bool {
+	s.storage.Set(os.Getenv("SESSION_SECRET"), data)
+	if err := s.storage.Save(); err != nil {
+		log.Printf("error in saving session data: %s", err)
+		return false
+	}
+
+	return true
+}
+
+func (s *Session) GetSessionData(message string) interface{} {
+	data := s.storage.Get(message)
+	if err := s.storage.Save(); err != nil {
+		log.Printf("error in getting session data: %s", err)
+		return nil
+	}
+	return data
+}
+
+func (s *Session) RemoveSessionData(message string) bool {
+	s.storage.AddFlash(message)
+	if err := s.storage.Save(); err != nil {
+		log.Printf("error in removing session data: %s", err)
+		return false
+	}
+
+	return true
+}
+
+func (s *Session) SetFlashMessage(message string) bool {
+	s.storage.AddFlash(message)
+	if err := s.storage.Save(); err != nil {
+		log.Printf("error in SetFlashMessage saving session: %s", err)
+		return false
+	}
+
+	return true
+}
+
+func (s *Session) GetFlashMessage(c *gin.Context) []interface{} {
+	flashes := s.storage.Flashes()
 	if len(flashes) != 0 {
-		if err := session.Save(); err != nil {
+		if err := s.storage.Save(); err != nil {
 			log.Printf("error in flashes saving session: %s", err)
+			return nil
 		}
 	}
 	return flashes
