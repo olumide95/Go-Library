929a6b6f9c37f416eca5be3bfbb231752c751cc0
Olumide Olugbemiro <olugbemiro.olumide@gmail.com>
2023-01-19 07:43:04 +0100

    updated book repository

--- repository/book_repository.go
+++ repository/book_repository.go
@@ -14,51 +14,51 @@ func NewBookRepository(DB *gorm.DB) models.BookRepository {
 	return &bookRepository{DB}
 }
 
-func (ur *bookRepository) Create(book *models.Book) error {
+func (br *bookRepository) Create(book *models.Book) error {
 
-	result := ur.database.Create(book)
-
-	return result.Error
-}
-
-func (ur *bookRepository) CreateBulk(book *[]models.Book) error {
-
-	result := ur.database.CreateInBatches(book, 100)
+	result := br.database.Create(book)
 
 	return result.Error
 }
 
-func (ur *bookRepository) Update(id uint, book *models.Book) (int64, error) {
+func (br *bookRepository) CreateBulk(book *[]models.Book) error {
 
-	result := ur.database.Where("id = ?", id).Updates(book)
+	result := br.database.CreateInBatches(book, 100)
+
+	return result.Error
+}
+
+func (br *bookRepository) Update(id uint, book *models.Book) (int64, error) {
+
+	result := br.database.Where("id = ?", id).Updates(book)
 
 	return result.RowsAffected, result.Error
 }
 
-func (ur *bookRepository) GetByID(id uint) (models.Book, error) {
+func (br *bookRepository) GetByID(id uint) (models.Book, error) {
 	var book models.Book
-	result := ur.database.First(&book, "id = ?", id)
+	result := br.database.First(&book, "id = ?", id)
 
 	return book, result.Error
 }
 
-func (ur *bookRepository) GetByIDForUpdate(id uint) (models.Book, error) {
+func (br *bookRepository) GetByIDForUpdate(id uint) (models.Book, error) {
 	var book models.Book
-	result := ur.database.Clauses(clause.Locking{Strength: "UPDATE"}).First(&book, "id = ?", id)
+	result := br.database.Clauses(clause.Locking{Strength: "UPDATE"}).First(&book, "id = ?", id)
 
 	return book, result.Error
 }
 
-func (ur *bookRepository) Delete(id []uint) (int64, error) {
+func (br *bookRepository) Delete(id []uint) (int64, error) {
 	var book models.Book
-	result := ur.database.Delete(&book, id)
+	result := br.database.Delete(&book, id)
 
 	return result.RowsAffected, result.Error
 }
 
-func (ur *bookRepository) All() ([]models.Book, error) {
+func (br *bookRepository) All() ([]models.Book, error) {
 	var books []models.Book
-	result := ur.database.Find(&books)
+	result := br.database.Find(&books)
 
 	return books, result.Error
 }
