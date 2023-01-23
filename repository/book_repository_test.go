bb7cbec9094f09e6f58f54a1ebcc199cc43a95a4
Olumide Olugbemiro <olugbemiro.olumide@gmail.com>
2023-01-23 06:46:04 +0100

    added repository test and test suite

---
+++ repository/book_repository_test.go
@@ -0,0 +1,33 @@
+package repository_test
+
+import (
+	"github.com/olumide95/go-library/models"
+	"github.com/olumide95/go-library/repository"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Repository", func() {
+	var repo models.BookRepository
+	BeforeEach(func() {
+		repo = repository.NewBookRepository(Db)
+		err := Db.AutoMigrate(&models.Book{})
+		Ω(err).To(Succeed())
+	})
+
+	It("Creates a book record", func() {
+		book := models.Book{Title: "Test Title", Author: "Test Author", Quantity: 2}
+		err := repo.Create(&book)
+		Ω(err).To(Succeed())
+	})
+
+	It("List all book records", func() {
+		book := models.Book{Title: "Test Title 1", Author: "Test Author", Quantity: 2}
+		repo.Create(&book)
+
+		books, err := repo.All()
+		Ω(err).To(Succeed())
+		Ω(books).To(HaveLen(1))
+		Ω(books[0].Title).To(Equal("Test Title 1"))
+	})
+})
---
+++ repository/repository_suite_test.go
@@ -0,0 +1,34 @@
+package repository_test
+
+import (
+	"testing"
+
+	"github.com/olumide95/go-library/bootstrap"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"gorm.io/gorm"
+)
+
+func TestRepository(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Docker Suite")
+}
+
+var Db *gorm.DB
+var cleanupDocker func()
+
+var _ = BeforeSuite(func() {
+	// setup *gorm.Db with docker
+	Db, cleanupDocker = bootstrap.SetupGormWithDocker()
+})
+
+var _ = AfterSuite(func() {
+	// cleanup resource
+	cleanupDocker()
+})
+
+var _ = BeforeEach(func() {
+	// clear db tables before each test
+	err := Db.Exec(`DROP SCHEMA public CASCADE;CREATE SCHEMA public;`).Error
+	Ω(err).To(Succeed())
+})
